bfd448b110761f5be915a119f39ccbd5d8a1089f
JINHAO LIU <jinhao@JINHAOs-MacBook-Pro.local>
2018-11-13 15:08:24 -0800

    jinhaoLab3

---
+++ manage.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route('/jinhaoLab3')
+def jinhaoLab3():
+	return 'This is Jinhao Liu lab3'
