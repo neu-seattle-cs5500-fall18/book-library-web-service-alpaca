ff1161db18df9955c5e9a9040bed5533aacaa050
JINHAO LIU <jinhao@JINHAOs-MacBook-Pro.local>
2018-11-13 17:16:43 -0800

    loan model

---
+++ models/loan_model.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Loan(db.Model):
+
+    LoanId = db.Column(db.Integer, primary_key=True)
+    BookId = db.Column(db.Integer, db.ForeignKey(Book.BookId), nullable=False)
+    BorrowerId = db.Column(db.Integer, db.Foreignkey(User.UserId), nullable=False)
+    Due = db.Column(db.DateTime, unique=False, nullable=True)
+    ReturnDate = db.Column(db.DateTime, unique=False, nullable=True)
+
+    @property
+    def serialize(self):
+        res = {
+            'LoanId': self.LoanId,
+            'BookId': self.BookId,
+            'BorrowerId': self.BorrowerId,
+            'Due': self.Due._str_(),
+            'ReturnDate': self.ReturnDate._str_(),
+        }
+        return res
+
