ffe6480f27e7ff63f8973557ce505000a01c11b7
anransu <su.an@husky.neu.edu>
2018-11-19 21:28:20 -0800

    added bookToNotes

--- API/book.py
+++ API/book.py
@@ -4,10 +4,11 @@ from models.book_model import *
 
 book_bp = Blueprint('book', __name__)
 
-DB_CONNECT = "postgresql://Anran:123456@localhost/library"
-engine = create_engine(DB_CONNECT)
+#DATABASE_URL = 'postgres://iltlntsqhnuuiu:b450237a15174db6eddf51d8af1e8fc1a8eb71509c6277560f04c1c876c4542c@ec2-54-163-230-178.compute-1.amazonaws.com:5432/dhdp0qr21t6ev'
+DB_LOCAL = ''
+engine = create_engine(DB_LOCAL, echo=True)
 
-@book_bp.route("/add_book")
+@book_bp.route("/add_book", methods=['POST'])
 def add_book():
 	book_id = request.args.get("id")
 	book_author = request.args.get("author")
@@ -21,16 +22,16 @@ def add_book():
 	connection.close()
     return "initial commit AnranBookBranch"
 
-@book_bp.route("/search_book")
+@book_bp.route("/search_book", methods=['GET'])
 def search_book():
  	return "search book"
 
 
-@book_bp.route("/update_book")
+@book_bp.route("/update_book", methods=['PUT'])
 def update_book():
 	return "update book"
 
-@book.bp.route("/delete_book")
+@book.bp.route("/delete_book", methods=['DELETE'])
 def delete_book():
 	return "delete book"
 
--- models/book_model.py
+++ models/book_model.py
@@ -5,13 +5,14 @@ db = SQLAlchemy()
 class Book(db.Model):
 
     book_id = db.Column(db.String, primary_key=True)
-    book_author = db.Column(db.String,nullable=False)
+    book_author = db.Column(db.String, nullable=False)
     book_title = db.Column(db.String, nullable=False)
     book_year = db.Column(db.Integer, nullable=False)
-    book_genre = db.Column(db.String,nullable=False)
+    book_genre = db.Column(db.String, nullable=False)
+    book_note = db.Column()
 
     @property
-    def jsonfy(self):
+    def serialize(self):
         return {
             'id': self.book_id,
             'author': self.book_author,
@@ -19,3 +20,18 @@ class Book(db.Model):
             'year': self.book_year,
             'genre': self.book_genre
         }
+
+class BookToNotes(db.Model):
+
+    id = db.Column(db.String, primary_key=True)
+    book_id = db.Column(db.String, nullable=False)
+    note_id = db.Column(db.String, nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+        'id':self.id,
+        'book_id':self.book_id,
+        'note_id':self.note_id
+        }
+    
