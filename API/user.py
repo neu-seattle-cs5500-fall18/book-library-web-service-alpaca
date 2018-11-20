486fe4de4c7e0f7edde45155805c9e22e3414dc5
haoxiangma <mahomehi@126.com>
2018-11-19 20:30:39 -0800

    add main page

--- API/user.py
+++ API/user.py
@@ -2,6 +2,10 @@ from flask import Blueprint
 
 user_bp = Blueprint('user', __name__)
 
+@user_bp.route('/')
+def main_page():
+    return 'Main Page!'
+
 @user_bp.route("/user")
 def func():
     pass
