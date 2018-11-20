586f31649bb55a1fedbcccc4d7ac3e11b724ae9f
haoxiangma <mahomehi@126.com>
2018-11-19 20:40:11 -0800

    add bp

--- manage.py
+++ manage.py
@@ -1,17 +1,17 @@
 from flask import Flask
-# from API.book import book_bp
-# from API.booklist import book_list_bp
-# from API.loan import loan_bp
-# from API.user import user_bp
+from API.book import book_bp
+from API.booklist import book_list_bp
+from API.loan import loan_bp
+from API.user import user_bp
 
 # init application
 app = Flask(__name__)
 
 # register blueprints for different resources
-# app.register_blueprint(book_bp)
-# app.register_blueprint(book_list_bp)
-# app.register_blueprint(loan_bp)
-# app.register_blueprint(user_bp)
+app.register_blueprint(book_bp)
+app.register_blueprint(book_list_bp)
+app.register_blueprint(loan_bp)
+app.register_blueprint(user_bp)
 
 @app.route('/')
 def main_page():
