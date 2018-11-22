1f59558518b7b7227835dac643d3f3b33ac99d12
Anran Su <su.an@husky.neu.edu>
2018-11-21 17:08:21 -0800

    added add_book

--- application/API/book.py
+++ application/API/book.py
@@ -1,26 +1,60 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from application import db
+from application.models.book_model import Book
+import json
+
 book_bp = Blueprint('book', __name__)
 
-#insert
-# db.session.add()
-# db.session.commit()
+@book_bp.route('/add_book', methods=['POST'])
+def add_book():
+    args = json.loads(request.get_data())  # get post args and trans to json format
+    try:
+        book_author = args['book_author']
+        book_title = args['book_title']
+        book_year = args['book_year']
+        book_genre = args['book_genre']
+        '''
+        book_author = "Conan"
+        book_title = "Sherlock"
+        book_year = 1890
+        book_genre = "Mystery"
+        '''
+    except:
+        return jsonify({
+            'message': 'invalid input arguments!'
+        }), 400
+
+    book = Book(book_author, book_title, book_year, book_genre)
+    db.session.add(book)
+    db.session.commit()
+    return jsonify({
+        'message': 'book inserted'
+    }), 201
 
 
-# @book_bp.route("/add_book")
-# def add_book():
-# 	book_id = request.args.get("id")
-# 	book_author = request.args.get("author")
-# 	book_title = request.args.get("title")
-# 	book_year = request.args.get("releaseYear")
-# 	book_genre = request.args.get("genre")
-# 	data = (book_id,book_author,book_title,book_year,book_genre)
-# 	insert_stmt = "INSERT INTO book (id,author,title,year,genre) VALUES (%s,%s,%s,%s,%s);"
-# 	connection = engine.connect()
-# 	connection.execute(insert_stmt,data)
-# 	connection.close()
+@book_bp.route('/delete_book', methods=['DELETE'])
+def delete_book():
+    args = json.loads(request.get_data())
+    try:
+        target_id = args['book_id']
+    except:
+        return jsonify({
+            'message': 'invalid input argument'
+        }),400
 
-# 	return "initial commit AnranBookBranch"
+    try:
+        book_to_delete = db.session.query(Book).filter_by(id=target_id).first()
+    except:
+        return jsonify({
+            'message': 'no such book to delete'
+        }),401
 
-# 	return "initial commit AnranBookBranch"
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return jsonify({
+        'message': 'book deleted'
+    }), 200
 
+@book_bp.route('/search_book', methods=['GET'])
+def search_book():
+    return "this is search book"
--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 # init application
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://iltlntsqhnuuiu:b450237a15174db6eddf51d8af1e8fc1a8eb71509c6277560f04c1c876c4542c@ec2-54-163-230-178.compute-1.amazonaws.com:5432/dhdp0qr21t6ev'
-
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://iltlntsqhnuuiu:b450237a15174db6eddf51d8af1e8fc1a8eb71509c6277560f04c1c876c4542c@ec2-54-163-230-178.compute-1.amazonaws.com:5432/dhdp0qr21t6ev'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Anran:123456@localhost:5432/library'
 db = SQLAlchemy(app)
 # register blueprints for different resources
