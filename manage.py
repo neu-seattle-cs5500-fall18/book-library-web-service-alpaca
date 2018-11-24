0f56763547ed64d23171c9b3f54097ee2e4219ec
haoxiangma <mahomehi@126.com>
2018-11-24 13:04:05 -0800

    reconstruct with flask_restplus.

--- manage.py
+++ manage.py
@@ -1,32 +1,19 @@
-# from flask import Flask
-from application.API.book import book_bp
-from application.API.booklist import book_list_bp
-from application.API.loan import loan_bp
-from application.API.user import user_bp
-from application.API.note import note_bp
-#
-# # init application
-# app = Flask(__name__)
-#
-# # register blueprints for different resources
-# app.register_blueprint(book_bp)
-# app.register_blueprint(book_list_bp)
-# app.register_blueprint(loan_bp)
-# app.register_blueprint(user_bp)
-
 from application import app
+from flask_restplus import Api
+from application.API.booklist import book_list_apis
 
-app.register_blueprint(book_bp)
-app.register_blueprint(note_bp)
-app.register_blueprint(book_list_bp)
-app.register_blueprint(loan_bp)
-app.register_blueprint(user_bp)
+# init API
+book_library_service_api = Api(
+    title='Book Library Service API',
+    version='1.0',
+    description='Book Library Service APIs developed by Team Alpaca.',
+)
 
-@app.route('/')
-def main_page():
-    return 'Main Page!!!'
+# add individual namespaces to the API
+book_library_service_api.add_namespace(ns=book_list_apis, path='/booklist')
+
+# combine the application with the API
+book_library_service_api.init_app(app)
 
 if __name__ == '__main__':
-    host = 'localhost'
-    port = 9000
-    app.run(host=host, port=port, debug=True)
+    app.run(debug=True)
