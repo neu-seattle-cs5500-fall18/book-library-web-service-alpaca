4840d027499a3782c7c77dc6234672f30e79980c
Song Zhe <songzhewhu@gmail.com>
2018-12-08 15:38:27 -0800

    Add alert specific user API based on userId(POST method)

--- application/API/useralert.py
+++ application/API/useralert.py
@@ -1,14 +1,13 @@
 from application import db
 from application.models.user_model import User
 from application.models.loan_model import Loan
-from application.models.book_model import Book
-
 from flask_restplus import Namespace, Resource, reqparse
 import time
 
 
 user_alert_apis = Namespace('User Alert API', description='User Alert API')
-
+parser = reqparse.RequestParser()
+parser.add_argument('user_id', help='user id')
 @user_alert_apis.route('/')
 class UserAlert(Resource):
     @user_alert_apis.doc(responses={200: 'Success', 400: 'Error'})
@@ -25,4 +24,29 @@ class UserAlert(Resource):
             'user_id_list': user_id_list
         }
 
+    def post(self):
+        args = parser.parse_args()
+        try:
+            user_id = args['user_id']
+        except:
+            return {
+                'message':'Please provide the user_id!'
+            }, 400
+        if user_id is None:
+            return {
+                'message':'Please provide the user_id!'
+            }, 400
+        curr_time = time.strftime("%Y-%m-%d", time.localtime())
+        books = db.session.quer(Loan).filter(Loan.user_id == user_id)\
+                                        .filter(Loan.due<=curr_time).all()
+        book_ids = [book.id for book in books]
+        if len(book_ids) > 0:
+            return {
+                'message': 'This user need to return his loaned book that passed the due already!',
+                'book_ids': book_ids
+            }, 200
+        else:
+            return {
+                'message': 'This user has returned all loaned books in time!'
+            }, 200
 
